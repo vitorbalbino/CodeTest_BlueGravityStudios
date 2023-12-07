ac9d003a4865aa1b1e42270bc32ba708f156edd6
Vitor Balbino de Castro <vitor.balbino.gd@gmail.com>
2023-12-07 14:25:59 -0300

    Interactions

---
+++ ClothesShop/Assets/App/Interactions/Interaction.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEditor.Progress;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] UnityEvent UEvent;
+
+    public string _actionName = "";
+    public string ActionName { get { return _actionName; } private set { _actionName = value; } }
+
+    public void EnableInteraction()
+    {
+        CanvasInput.Sgt.AddInteraction(this);
+    }
+
+    public void DisableInteraction()
+    {
+        CanvasInput.Sgt.RemoveInteraction(this);
+    }
+
+    internal void Invoke()
+    {
+        UEvent.Invoke();
+    }
+}
